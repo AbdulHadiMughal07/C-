2260013dc1cc66e82619cbdd1a817ad7bc3b1a93
AbdulHadiMughal07 <153829747+AbdulHadiMughal07@users.noreply.github.com>
2024-10-16 18:32:39 +0500

    Add Or Show

---
+++ Add Or Show.cs
@@ -0,0 +1,41 @@
+﻿namespace myapp
+{
+    class Program
+    {
+        static List<string> lisname = new List<string>();
+
+        static void Main() { 
+            while (true)
+            {
+                Console.WriteLine("1. Add Data\n 2. Show Data");
+
+                int userinput = Convert.ToInt32(Console.ReadLine());
+                
+                if(userinput == 1)
+                {
+                    Adddata();
+                }
+                else
+                {
+                    Showdata();
+                }
+
+            }
+
+        }
+
+        static void Adddata()
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            lisname.Add(name);
+        }
+        static void Showdata()
+        {
+            foreach (string names in lisname)
+            {
+                Console.WriteLine($"name is : {names}");
+            }
+        }
+    }
+}
