ad49b93d154592a733018732d9e57a203c945ae2
AbdulHadiMughal07 <153829747+AbdulHadiMughal07@users.noreply.github.com>
2024-10-18 18:53:44 +0500

    Done

---
+++ Multipale Types Data Add (Class & Object).cs
@@ -0,0 +1,59 @@
+﻿namespace myapp
+{
+    class Program
+    {
+        static List<Student> listname = new List<Student>();
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Add Data\n 2. Show Data");
+
+                int userinput = Convert.ToInt32(Console.ReadLine());
+
+                if (userinput == 1)
+                {
+                    Student.Adddata();
+                }
+                else
+                {
+                    Student.Showdata();
+                }
+
+            }
+
+        }
+        class Student
+        {
+
+
+            public int id { get; set; }
+            public string name { get; set; }
+
+            public static void Adddata()
+            {
+
+                Student std = new Student();
+
+                Console.WriteLine("Enter ID");
+                std.id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Name");
+                std.name = Console.ReadLine();
+
+                listname.Add(std);
+            }
+
+            public static void Showdata()
+            {
+                foreach (Student abc in listname)
+                {
+                    Console.WriteLine($"ID is : {abc.id}\n name is : {abc.name}");
+                }
+            }
+        }
+
+   
+    }
+}
